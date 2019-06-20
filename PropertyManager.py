5bdb6a2b7e6a41dd0f4949a93772f5d415ed6b3d
Gregory Rubinstein <Gregory.E.Rubinstein@gmail.com>
2019-06-20 09:23:07 -0500

    Create PropertyManager.py
    initial commit for the PropertyManager class.

---
+++ PropertyManager.py
@@ -0,0 +1,7 @@
+class PropertyManager:
+    def __init__(self, pStreetAddr, pCity = "Philadelphia", pState = "PA", pZip, pAptCount):
+        self.pAptCount = pAptCount
+        self.pCity = pCity
+        self.pState = pState
+        self.pStreetAddr = pStreetAddr
+        self.pZip = pZip
