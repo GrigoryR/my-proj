570f8af25992ec62f837090b7408c9fe39ba6bbf
Gregory Rubinstein <Gregory.E.Rubinstein@gmail.com>
2019-06-20 09:58:53 -0500

    Create Apartment.py
    initial commit for Apartment class

---
+++ Apartment.py
@@ -0,0 +1,6 @@
+class Apartment:
+    def __init__(self, pID, aptNo, rent, isVacant = False):
+        self.pID = pID
+        self.aptNo = aptNo
+        self.rent = rent
+        self.isVacant = isVacant
