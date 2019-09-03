224ee71df34d3d373946249a0562faec9732f751
Gregory Rubinstein <Gregory.E.Rubinstein@gmail.com>
2019-09-03 17:00:23 -0400

    added getters and setters
    added getters and setters for the properties

--- Apartment.py
+++ Apartment.py
@@ -4,3 +4,35 @@ class Apartment(House): #Apartment inherits from House
         self.aptNo = aptNo
         self.rent = rent
         self.isVacant = isVacant
+        
+    @property
+    def pID(self):
+        return self._pID
+    
+    @setter.pID
+    def pID(self, pID):
+        self._pID = pID
+        
+    @property
+    def aptNo(self):
+        return self._aptNo
+    
+    @setter.pID
+    def aptNo(self, aptNo):
+        self._aptNo = aptNo
+        
+    @property
+    def rent(self):
+        return self._rent
+    
+    @setter.rent
+    def pID(self, rent):
+        self._rent = rent
+        
+    @property
+    def isVacant(self):
+        return self._isVacant
+    
+    @setter.isVacant
+    def isVacant(self, isVacant):
+        self._isVacant = isVacant
