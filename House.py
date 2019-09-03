ae5835519aeff68c8f92984404770ec33f3372aa
Isaac-AR <52054455+Isaac-AR@users.noreply.github.com>
2019-09-03 16:59:32 -0400

    Update House.py

--- House.py
+++ House.py
@@ -6,3 +6,42 @@ class House:
         self.pState = pState
         self.pStreetAddr = pStreetAddr
         self.pZip = pZip
+    @property
+    def p(self):
+        return self._p
+    @pID.setter
+    def pID(self,pID):
+        self._pID = pID
+    
+    @property
+    def pAptCount(self):
+        return self._pAptCount
+    @pAptCount.setter
+    def pAptCount(self,pAptCount)
+        self._pAptCount = pAptCount
+    @property
+    def pCity(self):
+        return self._pCity
+    @pCity.setter
+    def pCity(self,pCity)
+        self._pCity = pCity
+    @property
+    def pState(self):
+        return self._pState
+    @pState.setter
+    def pState(self,pState)
+        self._pState = pState
+    @property
+    def pStreetAddr(self):
+        return self._pStreetAddr
+    @pStreetAddr.setter
+    def pStreetAddr(self,pStreetAddr)
+        self._pStreetAddr = pStreetAddr
+    @property
+    def pZip(self):
+        return self._pZip
+    @pZip.setter
+    def pZip(self,pZip)
+        self._pZip = pZip
+        
+    
