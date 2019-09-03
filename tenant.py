a8b2b44012035a0958083dfca33ef60dff58774c
Gregory Rubinstein <Gregory.E.Rubinstein@gmail.com>
2019-09-03 18:16:14 -0400

    added getters and setters for the properties

--- tenant.py
+++ tenant.py
@@ -1,3 +1,29 @@
 class Tenant:
     def __init__(self, TenantId, FirstName, LastName):
-        pass
+        self._TenantId = TenantId
+        self._FirstName = FirstName
+        self._LastName = LastName
+    
+    @property
+    def TenantId(self):
+        return self._TenantId
+    
+    @TenantId.setter
+    def TenantId(self, TenantId):
+        self._TenantId = TenantId
+    
+    @property
+    def FirstName(self):
+        return self._FirstName
+    
+    @FirstName.setter
+    def FirstName(self, FirstName):
+        self._FirstName = FirstName
+    
+    @property
+    def LastName(self):
+        return self._LastName
+    
+    @LastName.setter
+    def LastName(self, LastName):
+        self._LastName = LastName
