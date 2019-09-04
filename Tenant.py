818cbab418d0fcaf1f5a16e5d734c10aac219df7
Gregory Rubinstein <Gregory.E.Rubinstein@gmail.com>
2019-09-04 12:25:50 -0400

    added more properties and function placeholders
    added the following properties: 
    MoveInDate - date when tenant moved in
    MoveOutDate - date when tenant moved out
    CurrentTenant - True if the tenant still lives at the property, False if tenant has moved out
    
    and 
    
    function placeholders
    AddNewTenant(), ArchivePastTenant(), GetAllTenants(), GetCurrentTenants(), GetTenantById(TenantId), UpdateTenant(Tenant)
    
    and also a parameterless constructor

--- Tenant.py
+++ Tenant.py
@@ -4,6 +4,9 @@ class Tenant:
         self._FirstName = FirstName
         self._LastName = LastName
     
+    def __init__(self):
+        pass
+    
     @property
     def TenantId(self):
         return self._TenantId
@@ -27,3 +30,46 @@ class Tenant:
     @LastName.setter
     def LastName(self, LastName):
         self._LastName = LastName
+    
+    @property
+    def MoveInDate(self):
+        return self._MoveInDate
+        
+    @MoveInDate.setter
+    def MoveInDate(self, MoveInDate):
+        self._MoveInDate = MoveInDate
+    
+    @property
+    def MoveOutDate(self):
+        return self._MoveOutDate
+        
+    @MoveOutDate.setter
+    def MoveOutDate(self, MoveOutDate):
+        self._MoveOutDate = MoveOutDate
+    
+    @property
+    def CurrentTenant(self):
+        if MoveOutDate == None:
+            self._CurrentTenant = True
+        else:
+            self._CurrentTenant = False
+        return self._CurrentTenant
+    
+    #Functionality
+    def AddNewTenant():
+        pass
+    
+    def ArchivePastTenant():
+        pass
+    
+    def GetAllTenants():
+        pass
+    
+    def GetCurrentTenants():
+        pass
+    
+    def GetTenantById(TenantId):
+        pass
+    
+    def UpdateTenant(Tenant):
+        pass
